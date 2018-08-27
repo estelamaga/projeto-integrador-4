fa9b4c8dd4d2c7144c3d1e3a7aa7a416e6f9c3ff
francorufino <campbell.andreza@gmail.com>
2018-08-27 19:13:22 -0300

    navbar topo

---
+++ footer-navbar.php
@@ -0,0 +1,43 @@
+<!--INICIO DO FOOTER -->
+<footer>
+<div class="container-fluid footer-container">
+	<div class="row footer">
+	<div class="col-md-3">
+		<ul>
+	    <li><a href="#">Home</a></li>
+	    <li><a href="#">About</li>
+	    <li><a href="#">Contact</li>
+			<li><a href="#">Helping Others</li>
+	    <li><a href="#">FAQ</li>
+	  </ul>
+	</div>
+
+	<div class="col-md-3">
+		<ul>
+			<li><a href="#">Your Account</li>
+			<li><a href="#">Your Orders</li>
+			<li><a href="#">Shipping Rates & Poilicies</li>
+			<li><a href="#">Return and Replacements</li>
+			<li><a href="#">Help</li>
+		</ul>
+	</div>
+		<div class="col-md-3">
+	<ul>
+		<li><a href="#">Sell on Green Heart</li>
+		<li><a href="#">Sell your services on Green Heart</li>
+		<li><a href="#">Sell your apps on Green Heart</li>
+		<li><a href="#">Advertise on Green Heart</li>
+		<li><a href="#">Learn More</li>
+	</ul>
+		</div>
+	<div class="col col-md-3">
+		<ul>
+			<li><a href="#">Payment Options</li>
+				<img src="images/payment.png" alt="">
+		</ul>
+
+	</div>
+	</div>
+</div>
+</footer>
+<!-- FIM DO FOOTER -->
