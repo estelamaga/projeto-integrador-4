f5e52de0d75c05c12d7a9e817c4d64cc176bd2d2
estelamaga <estelinha.mag@hotmail.com>
2018-08-30 20:49:26 -0300

    Adicionando login com estilo

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="estilo.css" rel="stylesheet">
+  </head>
+  <body>
+  <div class="container">
+    <form class="text-center">
+    <h1> Fazer login</h1>
+    <label for="">Email:</label>
+    <input type="text" name="Email" placeholder="Ex:pedro@hotmail.com" class="form-control">
+    <label for="">Senha:</label>
+    <input type="text" name="senha" value="******" class="form-control">
+    <button class="btn" type="button">Continuar </button>
+    <br>
+    <button  class="btn Facebook" type="button">Entrar com Facebook</button>
+    <p>Não tem cadastro? <a href="#">Cadastra-se</a>    </p>
+    </form>
+  </div>
+  </body>
+  </html>
