fa86628085ed034ffffe6feadacf747fe0264a54
fronthendy <hendy.almeida@hotmail.com>
2018-09-10 20:55:14 -0300

    ajuste header, footer e estrutura dos arquivos

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <input type="text" name="Email" placeholder="Ex:pedro@hotmail.com" class="form-control">
     <label for="">Senha:</label>
     <input type="text" name="senha" value="******" class="form-control">
-    <button class="btn" type="button">Continuar </button>
+    <button class="btn" type="button">Entrar </button>
     <br>
     <button  class="btn Facebook" type="button">Entrar com Facebook</button>
     <p>Não tem cadastro? <a href="#">Cadastra-se</a>    </p>
