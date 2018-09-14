1235beb01a046b3e59556c67dc8af5f7250e24b3
estelamaga <estelinha.mag@hotmail.com>
2018-09-14 19:55:36 -0300

    formulario, login, persistencia e upload de foto funcionando

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,52 @@
+<?php
+session_start();
+?>
+
+<?php
+if($_POST){
+
+    $erro=[];
+    foreach ($_POST as $key => $value) {
+      if ($value == ""){
+      $erro[] = "Campo $key em branco";
+    }
+  }
+
+  if($_POST['senha'] != $_POST['confirma_senha']){
+    $erro[] = "Senha e Confirmar Senha devem ser iguais";
+  }
+
+
+  if(count($erro) === 0){
+    // O formulario deve ser validado do lado do servidor.
+    $arquivo="usuarios.json";
+
+    if (file_exists($arquivo)) {
+      $dados=file_get_contents($arquivo);
+      $dados=json_decode($dados,true); // para array associativo // adicionei , true para tornar o array associativo// sem ele ele retorna um objeto.
+      $formdados=$_POST;
+      $nomeDoARquivo = $_FILES["fotoPerfil"]["name"];
+      $caminhoDaFoto = "images/fotosUsuario/".$nomeDoARquivo;
+      $guardaRetorno= move_uploaded_file($_FILES["fotoPerfil"]["tmp_name"], $caminhoDaFoto);
+      $formdados["caminhofoto"]=$caminhoDaFoto;
+      $dados["usuarios"][]=$formdados;
+      $dadosemjson=json_encode($dados); //json string
+     file_put_contents($arquivo,$dadosemjson);
+      header('Location: login.php?cadastro=true');
+    }else{
+      $dados=["usuarios"=>[]];
+      $dados["usuarios"][]=$_POST;
+      $formdados=json_encode($dados); //json string
+      file_put_contents($arquivo,$formdados);
+      echo "arquivo criado";
+    }
+  }
+
+}
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,7 +93,7 @@
       	<div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12"><!-- Container Geral -->
 
-                <form  action="validaCadastro.php" method="post" enctype="multipart/form-data"><!-- Inicio Form -->
+                <form  action="cadastro.php" method="post" enctype="multipart/form-data"><!-- Inicio Form -->
 
                   <div class="panel panel-default"><!-- Inicio Panel Geral -->
                       <div class="panel-heading"><!-- Titulo Panel -->
@@ -55,36 +104,49 @@
                       			<div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
                                 <div class="col-lg-5 col-md-6 col-sm-6">
                                   <label for="ex1">Nome:</label>
-                                  <input type="text" class="form-control" maxlength="20"  name="nome" placeholder="Informe seu nome" required><br>
+                                  <input type="text" class="form-control" id="ex1" maxlength="40"  name="nome" placeholder="Insira seu nome" value='<?php echo isset($_POST['nome']) ? ($_POST['nome']): "" ?>'><br>
                                 </div>
                       			</div><!-- Fim Div -->
 
                       			<div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
                               <div class="col-lg-5 col-md-6 col-sm-6">
-                                <label for="ex1">Email:</label>
-                                <input type="email" class="form-control" maxlength="50"  name="email" placeholder="usuario@usuario.com" required><br>
+                                <label for="ex2">Email:</label>
+                                <input type="email" class="form-control" id="ex2" maxlength="50"  name="email" placeholder="usuario@usuario.com" value='<?php echo isset($_POST['email']) ? ($_POST['email']): "" ?>'><br>
                               </div>
                             </div><!-- Fim Div -->
 
                             <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
                               <div class="col-lg-5 col-md-6 col-sm-6">
-                                <label for="ex1">Senha:</label>
-                                <input type="password" class="form-control" maxlength=15  name="senha" placeholder="Informe sua senha"  required><br>
+                                <label for="ex3">Senha:</label>
+                                <input type="password" class="form-control" id="ex3" maxlength=15  name="senha" placeholder="Informe sua senha"><br>
                               </div>
                             </div><!-- Fim Div -->
 
                             <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
                               <div class="col-lg-5 col-md-6 col-sm-6">
-                                <label for="ex1">Confirmar Senha:</label>
-                                <input type="password" class="form-control" maxlength=15  name="confirma_senha" placeholder="Confirme sua senha" required><br>
+                                <label for="ex4">Confirmar Senha:</label>
+                                <input type="password" class="form-control" id="ex4" maxlength=15  name="confirma_senha" placeholder="Confirme sua senha"><br>
                               </div>
                             </div><!-- Fim Div -->
 
                             <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
                               <div class="col-lg-5 col-md-6 col-sm-6">
-                                <label for="ex1">Faca upload da sua foto de perfil</label>
+                                <label for="ex1">Faça upload da sua foto de perfil</label>
                                 <input type="file" class="form-control"  name="fotoPerfil"><br>
                               </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                  <?php
+                                    if (isset($erro) && count($erro)) {
+                                      ?>
+                                      <div class="alert alert-danger">
+                                        <?php
+                                            echo "<b>Atenção:</b> <br>";
+                                            echo implode ("<br>", $erro);
+                                         ?>
+                                      </div>
+                                      <?php } ?>
                             </div>
 
                       			<div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Button -->
