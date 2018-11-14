cd2b15c86204f1298b95095b315a8ca002d17e15
francorufino <campbell.andreza@gmail.com>
2018-11-14 19:08:41 -0200

    alteracoes cadastro

--- laravel/app/Http/Controllers/CadastroController.php
+++ laravel/app/Http/Controllers/CadastroController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Usuarios;
 
 class CadastroController extends Controller
 {
     public function cadastro(){
       return view('cadastro');
+    }
+    public function cadastroPessoaFisica(Request $request){
+      $usuario = Usuarios::create([
+        'nome' => $request->input('nome'),
+        'sobrenome' => $request->input('sobrenome'),
+        'data_de_nascimento' => $request->input('data_de_nascimento'),
+        'telefone' => $request->input('telefone'),
+        'email' => $request->input('email'),
+        'senha' => $request->input('senha'),
+        'cpf_cnpj' => $request->input('cpf'),
+        'sexo' => $request->input('sexo')
+      ]);
+
+
+      $usuario->save();
     }
 }
--- laravel/resources/views/cadastro.blade.php
+++ laravel/resources/views/cadastro.blade.php
@@ -17,97 +17,163 @@
     		</ul>
         <div class="tab-content clearfix">
   			  <div class="tab-pane active" id="pessoa-fisica">
-            <h3>Content's background color is the same for the tab</h3>
+            <form  action="/cadastro/pessoa-fisica" method="post" enctype="multipart/form-data" class="row"><!-- Inicio Form -->
+              {{ csrf_field() }}
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Nome</label>
+                  <input type="text" class="form-control" id="nome" maxlength="40"  name="nome" placeholder="Insira seu nome"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Sobrenome</label>
+                  <input type="text" class="form-control" id="sobrenome" maxlength="40"  name="sobrenome" placeholder="Insira seu nome"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Data de Nascimento</label>
+                  <input type="date" class="form-control" id="data_de_nascimento" maxlength="40"  name="data_de_nascimento" placeholder="Insira seu nome"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">CPF</label>
+                  <input type="text" class="form-control" id="cpf" maxlength="40"  name="cpf" placeholder="Insira seu nome"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex2">Email</label>
+                  <input type="email" class="form-control" id="email" maxlength="50"  name="email" placeholder="usuario@usuario.com"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Sexo</label>
+                  <input type="text" class="form-control" id="sexo" maxlength="40"  name="sexo" placeholder="Insira seu nome"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex2">Telefone</label>
+                  <input type="text" class="form-control" id="telefone" maxlength="50"  name="telefone" placeholder="11962928739"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex3">Senha</label>
+                  <input type="password" class="form-control" id="senha" maxlength=15  name="senha" placeholder="Informe sua senha"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex4">Confirmar Senha</label>
+                  <input type="password" class="form-control" id="confirma_senha" maxlength=15  name="confirma_senha" placeholder="Confirme sua senha"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Faça upload da sua foto de perfil</label>
+                  <input type="file" name="fotoPerfil"><br>
+                </div>
+              </div>
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Button -->
+                <div class="col-lg-2 col-md-6 col-sm-3">
+                  <input id="espaco2" type="submit" class="btn btn-success btn-lg btn-block" style="margin-top:17px; outline:none;" value="Cadastrar">
+                </div>
+              </div><!-- Fim Button -->
+
+            </form><!-- Fim Form -->
   				</div>
   				<div class="tab-pane" id="pessoa-juridica">
-            <h3>We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
+            <form  action="/cadastro/pessoa-juridica" method="post" enctype="multipart/form-data" class="row"><!-- Inicio Form -->
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Nome da Empresa:</label>
+                  <input type="text" class="form-control" id="ex1" maxlength="40"  name="nome" placeholder="Insira seu nome" value='<?php echo isset($_POST['nome']) ? ($_POST['nome']): "" ?>'><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">CNPJ</label>
+                  <input type="text" class="form-control" id="cnpj" maxlength="40"  name="cnpj" placeholder="Insira seu nome" value='<?php echo isset($_POST['nome']) ? ($_POST['nome']): "" ?>'><br>
+                </div>
+            </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex2">Email:</label>
+                  <input type="email" class="form-control" id="ex2" maxlength="50"  name="email" placeholder="usuario@usuario.com" value='<?php echo isset($_POST['email']) ? ($_POST['email']): "" ?>'><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex2">Telefone:</label>
+                  <input type="text" class="form-control" id="ex5" maxlength="50"  name="telefone" placeholder="11962928739" value='<?php echo isset($_POST['telefone']) ? ($_POST['telefone']): "" ?>'><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex3">Senha:</label>
+                  <input type="password" class="form-control" id="ex3" maxlength=15  name="senha" placeholder="Informe sua senha"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex4">Confirmar Senha:</label>
+                  <input type="password" class="form-control" id="ex4" maxlength=15  name="confirma_senha" placeholder="Confirme sua senha"><br>
+                </div>
+              </div><!-- Fim Div -->
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
+                <div class="col-lg-5 col-md-6 col-sm-6">
+                  <label for="ex1">Faça upload da sua foto de perfil</label>
+                  <input type="file" name="fotoPerfil"><br>
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <?php
+                if (isset($erro) && count($erro)) {
+                  ?>
+                  <div class="alert alert-danger">
+                    <?php
+                    echo "<b>Atenção:</b> <br>";
+                    echo implode ("<br>", $erro);
+                    ?>
+                  </div>
+                <?php } ?>
+              </div>
+
+              <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Button -->
+                <div class="col-lg-2 col-md-6 col-sm-3">
+                  <input id="espaco2" type="submit" class="btn btn-success btn-lg btn-block" style="margin-top:17px; outline:none;" value="Cadastrar">
+                </div>
+              </div><!-- Fim Button -->
+
+            </form><!-- Fim Form -->
   				</div>
         </div>
       </div>
 
-      <form  action="cadastro.php" method="post" enctype="multipart/form-data" class="row"><!-- Inicio Form -->
 
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex1">Tipo de Pessoa:</label>
-            <select type="text" class="form-control" id="ex1" maxlength="40"  name="tipoDePessoa">
-              <option value="pf">Pessoa Fisica</option>
-              <option value="pj">Pessoa Juridica</option>
-            </select><br>
-          </div>
-        </div><!-- Fim Div -->
-
-
-        <!--INICIO JAVASCRIPT = ESPERAR O JAVASCRIPT PARA ACRESCENTAR ESSES DADOS NO FORMULARIO -->
-        <!-- private $sobrenome;
-        private $dataDeNascimento;
-        private $cpf;
-        private $cnpj;
-        private $sexo; -->
-        <!-- FIM JAVASCRIPT -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex1">Nome ou Nome da Empresa:</label>
-            <input type="text" class="form-control" id="ex1" maxlength="40"  name="nome" placeholder="Insira seu nome" value='<?php echo isset($_POST['nome']) ? ($_POST['nome']): "" ?>'><br>
-          </div>
-        </div><!-- Fim Div -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex2">Email:</label>
-            <input type="email" class="form-control" id="ex2" maxlength="50"  name="email" placeholder="usuario@usuario.com" value='<?php echo isset($_POST['email']) ? ($_POST['email']): "" ?>'><br>
-          </div>
-        </div><!-- Fim Div -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex2">Telefone:</label>
-            <input type="text" class="form-control" id="ex5" maxlength="50"  name="telefone" placeholder="11962928739" value='<?php echo isset($_POST['telefone']) ? ($_POST['telefone']): "" ?>'><br>
-          </div>
-        </div><!-- Fim Div -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex3">Senha:</label>
-            <input type="password" class="form-control" id="ex3" maxlength=15  name="senha" placeholder="Informe sua senha"><br>
-          </div>
-        </div><!-- Fim Div -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex4">Confirmar Senha:</label>
-            <input type="password" class="form-control" id="ex4" maxlength=15  name="confirma_senha" placeholder="Confirme sua senha"><br>
-          </div>
-        </div><!-- Fim Div -->
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Div -->
-          <div class="col-lg-5 col-md-6 col-sm-6">
-            <label for="ex1">Faça upload da sua foto de perfil</label>
-            <input type="file" name="fotoPerfil"><br>
-          </div>
-        </div>
-
-        <div class="col-md-12">
-          <?php
-          if (isset($erro) && count($erro)) {
-            ?>
-            <div class="alert alert-danger">
-              <?php
-              echo "<b>Atenção:</b> <br>";
-              echo implode ("<br>", $erro);
-              ?>
-            </div>
-          <?php } ?>
-        </div>
-
-        <div class="col-lg-12 col-md-12 col-sm-12"><!-- Inicio Button -->
-          <div class="col-lg-2 col-md-6 col-sm-3">
-            <input id="espaco2" type="submit" class="btn btn-success btn-lg btn-block" style="margin-top:17px; outline:none;" value="Cadastrar">
-          </div>
-        </div><!-- Fim Button -->
-
-      </form><!-- Fim Form -->
     </div><!-- Fim Panel Panel-Default -->
   </div><!-- Fim Conteiner Geral -->
 </div> <!-- Fim da Row -->
